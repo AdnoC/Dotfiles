3c200b5260cf8822d8b73c3e26c5a075dbf95804
AdnoC <adam.r.cutler@gmail.com>
2014-07-23 15:12:58 +0000

    More changes

--- vim/postinstall.sh
+++
@@ -1,5 +0,0 @@
-# Install plugins
-vim +qall
-# Fix line endings for jcommenter
-sed -e "s///g" ~/.vim/bundle/jcommenter.vim/plugin/jcommenter.vim > tmp
-mv tmp ~/.vim/bundle/jcommenter.vim/plugin/jcommenter.vim
