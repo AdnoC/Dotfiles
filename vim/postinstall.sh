ccafea62fca810ea6aa7cf3c0463ad4ef9d12d80
AdnoC <adam.r.cutler@gmail.com>
2014-08-05 20:53:08 +0000

    Changed jsctags and jcommenter. Syntax plugins lazily loaded.
    Changed to a jcommenter with correct line endings.
    Changed jsctags from the doctorjs version to one using tern.
    Syntax plugins are now only loaded when the correct file type is opened.

--- vim/postinstall.sh
+++ vim/postinstall.sh
@@ -1,5 +1,3 @@
 # Install plugins
 vim +qall
-# Fix line endings for jcommenter
-sed -e "s///g" ~/.vim/bundle/jcommenter.vim/plugin/jcommenter.vim > tmp
-mv tmp ~/.vim/bundle/jcommenter.vim/plugin/jcommenter.vim
+vim +NeoBundleCheck +qall
