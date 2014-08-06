425c0b8c1e811c06d349896c186609dbc8eda1b0
AdnoC <adam.r.cutler@gmail.com>
2014-08-06 16:55:07 -0400

    Annoying 'cannot find ____' warning messages only displayed first vim run

--- vim/install.sh
+++ vim/install.sh
@@ -2,7 +2,7 @@ if ! command -v ctags >/dev/null 2>&1; then
   if [ "$(uname)" == "Darwin" ]; then
     brew install ctags-exuberant
   elif [ "$(uname -s)" == "Linux" ]; then
-    if ! command -v apt-get >/dev/null 2>&1; then
+    if command -v apt-get >/dev/null 2>&1; then
       echo "Installing ctags from apt-get"
       sudo apt-get install exuberant-ctags
     fi
