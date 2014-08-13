445fc826f71c0c3526a1365389200fc7163ea480
AdnoC <adam.r.cutler@gmail.com>
2014-08-13 18:44:57 +0000

    Added YouCompleteMe support

--- vim/postinstall.sh
+++ vim/postinstall.sh
@@ -1,3 +1,20 @@
 # Install plugins
-vim +qall
 vim +NeoBundleCheck +qall
+
+if [ "$(uname -s)" == "Linux" ] || [ "$(uname)" == "Darwin" ]; then
+  # If the version of vim is too low for YouCompleteMe
+  if [ $(vim --version | grep -o '7\.[0-9]') -lt '7.4' ]; then
+    if [ "$(uname -s)" == "Linux" ]; then
+      if command -v apt-get >/dev/null 2>&1; then
+        sudo add-apt-repository ppa:fcwu-tw/ppa
+        sudo apt-get update
+        sudo apt-get install vim
+      fi
+    fi
+  fi
+
+  cd ~/.vim/bundle/YouCompleteMe
+  git submodule update --init --recursive
+  ./install.sh
+  cd -
+fi
