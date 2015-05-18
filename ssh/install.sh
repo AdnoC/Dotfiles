bb50eb83ee7790d3e47aca22b2f7c72e1f977ce5
AdnoC <adam.r.cutler@gmail.com>
2015-05-18 18:33:02 -0400

    Small ssh preferences fix

--- ssh/install.sh
+++ ssh/install.sh
@@ -1,5 +1,5 @@
-if [ ! -f ~/.ssh/rc ] && [ -z "$LINK_SSH_RC"]; then
-  if [ ! -d ~/.ssh ]; then
+if [ ! -f ~/.ssh/rc ]; then
+  if [ ! -d ~/.ssh ] && [ -z "$LINK_SSH_RC"]; then
     mkdir ~/.ssh
   fi
   link_file "$DOTFILES_ROOT"/ssh/rc.sym ~/.ssh/rc
