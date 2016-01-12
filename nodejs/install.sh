0938f9e3a55d4f4d9fd37a203b317a26387d80f8
AdnoC <adam.r.cutler@gmail.com>
2016-01-12 02:42:50 +0000

    Now installs latest nodejs for Ubuntu 12.04

--- nodejs/install.sh
+++ nodejs/install.sh
@@ -1,7 +1,14 @@
 if ! hasCommand "node"; then
   if isLinux; then
+    if [ "$( echo "$(cat /etc/lsb-release | grep -o -m 1 "[0-9]*\.[0-9]*") 12.04" | awk -f ~/dotfiles/script/version.awk)" -eq 0 ]; then
+      sudo apt-get -y install clang-3.4 libssl-dev curl 
+      CC=/usr/bin/clang-3.4
+      CXX=/usr/bin/clang-3.4
+      export CC
+      export CXX
+      curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
     # If there is no nodejs in the apt-get repos, add one
-    if ! apt-cache search nodejs | grep "nodejs -" >/dev/null 2>&1; then
+    elif ! apt-cache search nodejs | grep "nodejs -" >/dev/null 2>&1; then
       sudo apt-get -y install python-software-properties
       sudo apt-add-repository -y ppa:chris-lea/node.js
       sudo apt-get update
