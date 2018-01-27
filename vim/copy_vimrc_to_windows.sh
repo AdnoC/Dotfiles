83f2bb67c2cbef2bcd2944b6954b433047325fbf
AdnoC <adam.r.cutler@gmail.com>
2018-01-26 21:03:27 -0500

    Revamp windows copy script

--- vim/copy_vimrc_to_windows.sh
+++ vim/copy_vimrc_to_windows.sh
@@ -1,7 +1,35 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-# cp ~/.config/nvim/init.vim ~/WinUser/AppData/Local/nvim/init.vim
-# cp ~/.config/nvim/.vimrc_plugin ~/WinUser/AppData/Local/nvim/.vimrc_plugin
+curDir="$(pwd)"
+symlinkRelPath() {
+  hardFilePath="$1"
+  relSymPath="${hardFilePath#$curDir}"
+  hardName="$(basename "$relSymPath")"
+  fixedName="${hardName#symlink.}"
+  relDirPath="$(dirname "$relSymPath")/"
+  relPath="${relDirPath#/*/}"
+
+  echo "$relPath$fixedName" # Not prefixed with a '/'
+}
+
+WIN_VIM_HOME="$HOME"/WinUser/AppData/Local/nvim
+
+print_copy_info() {
+  echo "copied $1 to $2"
+}
+
+print_copy_info ~/.vimrc ~/WinUser/AppData/Local/nvim/init.vim
 cp ~/.vimrc ~/WinUser/AppData/Local/nvim/init.vim
-cp ~/.vimrc_plugin ~/WinUser/AppData/Local/nvim/.vimrc_plugin
-cp ~/dotfiles/vim/ginit.vim ~/WinUser/AppData/Local/nvim/ginit.vim
+
+for src in $(find "$(pwd)" -name 'symlink.*'); do
+  relDest="$(symlinkRelPath "$src")"
+  if [[ $relDest == .vim/* ]]; then
+    relDest="${relDest#.vim/}"
+  fi
+  dst="$WIN_VIM_HOME/$relDest"
+
+  mkdir -p "$(dirname "$dst")"
+
+  print_copy_info "$src" "$dst"
+  cp -rT "$src" "$dst"
+done
--- vim/link.sh
+++ vim/link.sh
@@ -3,6 +3,7 @@
 # Place neovim config in the right place
 mkdir -p "${XDG_CONFIG_HOME:=$HOME/.config}/nvim"
 
+link_file "$(readlink "$HOME"/.vimrc)" "$XDG_CONFIG_HOME"/nvim/init.vim
 for src in $(find "$DOTFILES_ROOT"/vim -name 'symlink.*'); do
   relDest="$(symlinkRelPath "$src")"
   if [[ $relDest == .vim/* ]]; then
